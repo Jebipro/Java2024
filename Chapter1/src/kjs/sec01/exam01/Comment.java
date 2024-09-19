af1adf87918240327d95214b03df758629cd2705
Jebipro <20224298@s.scnu.ac.kr>
2024-09-19 12:37:30 +0900

    Chapter1

---
+++ Chapter1/src/kjs/sec01/exam01/Comment.java
@@ -0,0 +1,14 @@
+package kjs.sec01.exam01;
+
+/*
+ * 작성자: 김진성
+ * 작성일: 2025.12.31
+ */
+public class Comment {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Java");
+	} //end of main
+}  //end of class
---
+++ Chapter1/src/kjs/sec01/exam01/Hello.java
@@ -0,0 +1,11 @@
+package kjs.sec01.exam01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+
+	}
+
+}
