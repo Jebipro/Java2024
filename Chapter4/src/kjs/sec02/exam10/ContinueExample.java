10afa03d4cd83a3162b0d9d2ed2f21dc9013df66
Jebipro <20224298@s.scnu.ac.kr>
2024-09-26 21:48:44 +0900

    Chapter 4

---
+++ Chapter4/src/kjs/sec02/exam10/ContinueExample.java
@@ -0,0 +1,13 @@
+package kjs.sec02.exam10;
+
+public class ContinueExample {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			if(i%2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+	}
+}
